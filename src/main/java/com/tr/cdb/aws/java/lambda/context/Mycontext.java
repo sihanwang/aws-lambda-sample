08684381af5df93e79abb2335ce1f0b720174137
sihanwang <jing.wang@jings-mbp.apac.ime.reuters.com>
2019-11-12 17:24:05 +0800

    no message

--- src/main/java/com/tr/cdb/aws/java/lambda/context/Mycontext.java
+++ src/main/java/com/tr/cdb/aws/java/lambda/context/Mycontext.java
@@ -3,7 +3,14 @@ package com.tr.cdb.aws.java.lambda.context;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Reader;
+import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -12,36 +19,65 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 public class Mycontext implements RequestStreamHandler {
 	
 	//The input payload must be valid JSON but the output stream does not carry such a restriction. Any bytes are supported.
+	//input 必须是json
 	public void handleRequest(InputStream is, OutputStream os, Context context) throws IOException
 	{
-		LambdaLogger logger = context.getLogger();
-		logger.log("Function name: " + context.getFunctionName() + "\n");
-		logger.log("Function name: " + context.getClientContext() + "\n");
-
-		Map<String, String> CustomMap = context.getClientContext().getCustom();
-
-		CustomMap.forEach(
-
-				(key, value) -> {
-					logger.log("Custom Map Key: " + key + ", Value:" + value+"\n");
-				}
-
-		);
-
-		Map<String, String> EnvironmentMap = context.getClientContext().getEnvironment();
 		
-		EnvironmentMap.forEach(
+		
+		StringBuffer sbResponse=new StringBuffer();
+		
+		LambdaLogger logger = context.getLogger();
+		
+		StringBuffer function_name=new StringBuffer("Function Name: ").append(context.getFunctionName()).append("\n");
+		logger.log(function_name.toString());
+		sbResponse.append(function_name);
+		
+		StringBuffer function_clientcontext=new StringBuffer("Function Client Context: ").append(context.getClientContext()).append("\n");
+		logger.log(function_clientcontext.toString());
+		sbResponse.append(function_clientcontext);
 
-				(key, value) -> {
-					logger.log("Custom Map Key: " + key + ", Value:" + value+"\n");
-				}
+		StringBuffer max_mem=new StringBuffer("Max mem allocated: ").append(context.getMemoryLimitInMB()).append("\n");
+		logger.log(max_mem.toString());
+		sbResponse.append(max_mem);
+		
+		StringBuffer time_remaining=new StringBuffer("Time remaining in milliseconds: ").append(context.getRemainingTimeInMillis()).append("\n");
+		logger.log(time_remaining.toString());
+		sbResponse.append(time_remaining);
+		
+		StringBuffer cloudwatch_stream=new StringBuffer("CloudWatch log stream name: ").append(context.getLogStreamName()).append("\n");
+		logger.log(cloudwatch_stream.toString());
+		sbResponse.append(cloudwatch_stream);		
+		
+		StringBuffer cloudwatch_group=new StringBuffer("CloudWatch log group name: ").append(context.getLogGroupName()).append("\n");
+		logger.log(cloudwatch_group.toString());
+		sbResponse.append(cloudwatch_group);		
+		
+		sbResponse.append("InputStream: ");		
+		
+		int bufferSize = 1024;
+		char[] buffer = new char[bufferSize];
+		StringBuilder out = new StringBuilder();
+		Reader in = new InputStreamReader(is, "UTF-8");
+		for (; ; ) {
+		    int rsz = in.read(buffer, 0, buffer.length);
+		    if (rsz < 0)
+		        break;
+		    out.append(buffer, 0, rsz);
+		}
+		
+		sbResponse.append(out).append("\n");
+		
+		System.getenv();
+		sbResponse.append("Environment variables (if any): ");	
+			
+		for (Entry<String,String> pair : System.getenv().entrySet())
+		{
+			sbResponse.append(pair.getKey()).append("--->").append(pair.getValue()).append("\n");
+		}
+		
+		OutputStreamWriter osw=new OutputStreamWriter(os,"utf-8");
 
-		);
-
-		logger.log("Max mem allocated: " + context.getMemoryLimitInMB() + "\n");
-		logger.log("Time remaining in milliseconds: " + context.getRemainingTimeInMillis() + "\n");
-		logger.log("CloudWatch log stream name: " + context.getLogStreamName() + "\n");
-		logger.log("CloudWatch log group name: " + context.getLogGroupName() + "\n");
-
+		osw.write(sbResponse.toString());
+		osw.flush();
 	}
 }
